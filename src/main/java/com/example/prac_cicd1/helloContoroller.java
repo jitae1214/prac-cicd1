d7ccc34fffd425b31ab183d1232baec812f1349b
jitae <jite1214@gmail.com>
2025-06-24 12:08:05 +0900

    dfdfd

--- src/main/java/com/example/prac_cicd1/helloContoroller.java
+++ src/main/java/com/example/prac_cicd1/helloContoroller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class helloContoroller {
     @GetMapping("/hello")
     public String hello() {
-        return "hello 55555    ^^^^  !!!!";
+        return "hello 55555    ^^^^  !!!!  !!!!!!!!!!!!!";
     }
 }
